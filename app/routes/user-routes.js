439313f59467af44e60eacc7ac632bb12e388934
Giuseppe <giuseppetutino1@gmail.com>
2022-09-19 17:45:35 +0200

    Logged endpoint fix 2

--- app/routes/user-routes.js
+++ app/routes/user-routes.js
@@ -6,7 +6,7 @@ module.exports = app => {
     // User routes
     router.get("/passwords", userController.passwords);
     router.post("/addPasswordItem", userController.addPasswordItem);
-    router.get("/legged", userController.logged);
+    router.get("/logged", userController.logged);
 
 
     app.use('/user', router);
